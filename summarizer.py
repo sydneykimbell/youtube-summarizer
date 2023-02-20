362eebbf498daa4ebdcb30041cc0dacc86bc0f85
Sydney Kimbell <sydneykimbell@c8-89-f3-dd-53-5f.wireless.temple.edu>
2023-02-19 22:25:33 -0500

    added summarizer function

--- summarizer.py
+++ summarizer.py
@@ -3,10 +3,12 @@ import sumy
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
-from gensim.summarization.summarizer import summarize
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.utils import get_stop_words
 
-video_id = "g6cjhUhrhY8"
+video_id = "F2Mx-u7auUs"
 
 def concat_transcript(d_script):
     str_script = ""
@@ -14,14 +16,29 @@ def concat_transcript(d_script):
         str_script += d['text'] + ' '
     return str_script
 
-def summarize(str_script):
-    gen_summary = summarize(str_script)
-    print(gen_summary)
+def summarize(text, num_sentences=3, algorithm='textrank'):
+    
+    parser = PlaintextParser.from_string(text, Tokenizer('english'))
+    
+    if algorithm == 'lexrank':
+        summarizer = LexRankSummarizer()
+    elif algorithm == 'lsa':
+        summarizer = LsaSummarizer()
+    elif algorithm == 'luhn':
+        summarizer = LuhnSummarizer()
+    else:
+        summarizer = TextRankSummarizer()
+        
+    summary = summarizer(parser.document, num_sentences)
+    
+    return summary
 
 
 def main():
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
     ct = concat_transcript(transcript)
-    summarize(ct)
+    summary = summarize(ct, num_sentences=3, algorithm="luhn")
+    for sentence in summary:
+        print(sentence)
 main()
 
